5f391272477754ea747fae7656cafeefe78e16ff
WheelsIT <daniel.wheeler+github@wheelsit.com.au>
2024-05-09 12:42:28 +1000

    Add Script Header
    Added Script header and updated comments in script.
    Redacted email addresses

--- Create&Update Guest Wifi Password.ps1
+++ Create&Update Guest Wifi Password.ps1
@@ -1,7 +1,30 @@
+################################################
+##                                            ##
+##  Aruba Instant AP Guest WiFi Cred Update   ##
+##              Script                        ##
+##                                            ##
+##  This script updates the guest WiFi        ##
+##  password and emails it to the It Team     ##
+##                                            ##
+##  Author: Daniel Wheeler                    ##
+##  Organisation: WheelsIT                    ##
+##  Date: May 8, 2024                         ##
+##  Version: 1.0                              ##
+##                                            ##
+##                                            ##
+################################################
+
+
+
+#Aruba admin creds are stored in Credential Manager and accessed as an object
+#Credentials can only be accessed by the Aruba Service account arubasvc and are encrypted amd not stored in plain text in memory
+
 $ArubaCreds = Get-StoredCredential -Target 'aruba'
-$GuestUsername = Guest123
+$GuestUsername = 'Guest'
 
 
+
+#Main function which calls sub functions
 function Main {
     Get-RandomPass
     Connect-Aruba
@@ -10,16 +33,18 @@ function Main {
 }
 
 
+
+#Create random password using Rest API to word dictionary hosted at herokuapp.com
 function Get-RandomPass {
     $Word = Invoke-RestMethod -Uri "https://random-word-api.herokuapp.com/word?number=1"
     $Word = (Get-Culture).TextInfo.ToTitleCase($Word)
     $Number = "{0:d3}" -f (Get-Random -Minimum 1 -Maximum 999)
     $Special = [char[]]"@#$!%&" | Get-Random
-    $GuestPassword = $Word + $Number + $Special
-    $GuestPassword
+    $global:GuestPassword = $Word + $Number + $Special
+    $global:GuestPassword
 }
 
-
+#Connect to Aruba IAP, Initiate SSH Session and supply commands to update Guest user
 function Connect-Aruba {
     Import-Module -Name Posh-SSH
     $session = New-SSHSession -ComputerName 192.168.101.200 -Credential $ArubaCreds -AcceptKey -Force
@@ -28,28 +53,18 @@ function Connect-Aruba {
     $SSHStream = New-SSHShellStream -Index 0
     start-sleep -s 2
     $SSHStream.WriteLine("configure")
-    start-sleep -s 2
-    $sshresult = $SSHStream.read()
-    start-sleep -s 2
-    $sshresult
+    start-sleep -s 3
+   
 
-    $SSHStream.WriteLine("user $GuestUsername $GuestPassword portal")
-    start-sleep -s 2
-    $sshresult = $SSHStream.read()
-    start-sleep -s 2
-    $sshresult
-    start-sleep -s 2
+    $SSHStream.WriteLine("user $GuestUsername $global:GuestPassword portal")
+    start-sleep -s 3
+
+
     $SSHStream.WriteLine("end")
-    start-sleep -s 2
-    $sshresult = $SSHStream.read()
-    start-sleep -s 2
-    $sshresult
-    start-sleep -s 2
+    start-sleep -s 3
+
     $SSHStream.WriteLine("commit apply")
-    start-sleep -s 2
-    $sshresult = $SSHStream.read()
-    start-sleep -s 2
-    $sshresult
+    start-sleep -s 3
 
 
     # close session
@@ -57,14 +72,14 @@ function Connect-Aruba {
 }
 
 
-
+#Send email with Updated Credentials to IT Team
 function Send-Email {
 
 
     $Smtp = 'brandservices-com-au.mail.protection.outlook.com'
     $Date = Get-Date -Format "MMMM yyyy"
     $Subject = 'Guest Wi-Fi Password for ' + $Date
-    $Body = "The Guest Wi-Fi Credentials for " + $Date + " are: `nUsername: " + $GuestUsername + "`nPassword: " + $GuestPassword 
+    $Body = "The Guest Wi-Fi Credentials for " + $Date + " are: `nUsername: " + $GuestUsername + "`nPassword: " + $global:GuestPassword 
     
     
     
